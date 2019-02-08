af090246e5bf7bef3e1062e4cb4e4b6b2f963493
Partha Sarathi Reddy Bijjam <partha@ecoihub.com>
2019-02-08 17:32:04 +0530

    Initial Commit

---
+++ AutoCompleteTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoCompleteTextField
+//
+//  Created by Partha Sarathi Reddy Bijjam on 08/02/19.
+//  Copyright © 2019 Partha Sarathi Reddy Bijjam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
